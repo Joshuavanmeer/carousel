2339e6d106b70709e8808fcc2e3a0ca3a8b8d718
SenorJos <joshuavanmeer@gmail.com>
2017-02-02 12:10:58 +0100

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    entry: {
+        index: __dirname + '/src/index.js',
+    },
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    }
+};
